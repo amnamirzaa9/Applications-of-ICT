d0b38d45b7d7e95aac4203668c236afaadf40fb8
bismahadil <bismahadil12@gmail.com>
2025-09-22 22:01:03 +0500

    Add files via upload

---
+++ Bismah Adil/task 2_arithmetic.py
@@ -0,0 +1,10 @@
+a=10
+b=3
+add=a+b
+print("Addition", a + b)
+print("Subtraction", a - b)
+print("Multiplication", a * b)
+print("Division", a / b)
+print("Floor division", a // b)
+print("mModulus", a % b)
+print("Exponent", a ** b)
